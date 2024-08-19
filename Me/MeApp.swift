aa8bc09a8d6db8909bc1ba5f9b290e7ba8831ef4
Christopher Chang <cchang6066@stu.d214.org>
2024-08-19 07:29:44 -0700

    Initial Commit

---
+++ Me/MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeApp.swift
+//  Me
+//
+//  Created by Christopher Chang on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
