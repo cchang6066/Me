538bd23773e0466c2c1ca9326a643c295a3df28a
Christopher Chang <cchang6066@stu.d214.org>
2024-08-21 07:32:17 -0700

    j

--- Me/ContentView.swift
+++ Me/ContentView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Text("ALL ABOUT ME")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+            Text("")
+           Text("Christopher")
+                .font(.title)
+            Text("")
+           Text("2 Truth and a Lie")
+                .font(.title)
+                .foregroundColor(.red)
+            Text("\n\n")
+                
+           Text("1. MY FAVORITE SPORTS IS TENNIS")
+//                .font(.title)
+            Text("")
+           Text("2. I HAVE DRIVER LICENSE")
+//                .font(.title)
+            Text("")
+           Text("3. I HAVE SISTER WHO GO TO PHS")
+//                .font(.title)
+            
+            
         }
         .padding()
     }
