ea8366b59c5089dd75e708bd887579b33da196b3
ChrisChiramel <cchiramel6081@stu.d214.org>
2024-08-23 08:49:00 -0500

    j

--- Me/File.swift
+++ Me/File.swift
@@ -11,7 +11,16 @@ struct File: View {
     
     var body: some View {
         VStack {
-            
+            Text("***All About***")
+                .font(Font.largeTitle)
+                .foregroundColor(.blue)
+            Text("**Chris Chiramel**")
+                .font(Font.headline)
+            Text("*Junior Year*")
+                .frame(maxWidth: .infinity, maxHeight: .infinity,alignment: .top)
+                .foregroundColor(.red)
+            Text("I do Cross county")
+                .font(Font.custom("Helvetica", fixedSize: 20))
         }
     }
 }
